9342aee34a2df3195bb4ea8cc73599e0075afd96
Eleven <eleveneat@gmail.com>
2014-10-11 00:18:53 +0800

    Fix some bugs, and add some methods of the class stack.But still has something unfinished.

--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -1,14 +1,11 @@
-
 #include "Stack.h"
 
-
 template<class T>
-Stack<T >::Stack(int MaxStackSize)
+Stack<T>::Stack(int MaxStackSize)
 {//Stack类构造函数
     MaxTop = MaxStackSize - 1;
     stack = new T [MaxStackSize];
     top = -1;
-    size = 0;
 }
 template<class T>
 T Stack <T>::Top() const
@@ -17,29 +14,38 @@ T Stack <T>::Top() const
     else return stack[top];
 }
 template<class T>
-Stack<T >& Stack< T>::Add(const T& x)
+void Stack<T>::Push(const T& x)
 {//添加元素x
     if (isFull())throw NoMem();
     stack[++top] = x;
-    return *this ;
 }
 template<class T>
-Stack<T >& Stack< T>::Delete(T & x)
+void Stack<T>::Pop(T & x)
 {//删除栈顶元素，并将其送入x
     if (isEmpty())throw OutOfBounds();
     x = stack[top--];
-    return *this ;
 }
 template<class T>
-int Stack <T>::Size()
+int Stack<T>::Size()
 {//返回堆栈的大小
-    size = top + 1;
-    return size;
+    return top + 1;
 }
 template<class T>
-Stack<T >& Stack< T>::inputStack(Stack <T>& otherStack)
-{
-    if (MaxTop < otherStack .Size() - 1)throw OutOfBounds();
-    ｝
-    
-    
+void Stack<T>::inputStack(Stack otherStack)
+{//输入一个堆栈
+    if (MaxTop < top + otherStack.top)throw OutOfBounds();
+    int tmpSize = otherStack.Size();
+    T *tmpSta = new T[tmpSize];
+    for(int i = 0; i < tmpSize; ++i) {
+	    tmpSta[i] = otherStack.Top();
+	    otherStack.Pop();
+    }
+    for(int i = tmpSize - 1; i < 0; --i) {
+	    this->Push(tmpSta[i]);
+    }
+    delete []tmpSta;
+}
+template<class T>
+Stack Stack<T>::outputStack() {
+	return *this;
+}
--- Stack/Stack.h
+++ Stack/Stack.h
@@ -1,4 +1,3 @@
-
 #ifndef _STACK_H_
 #define _STACK_H_
 template<class T>
@@ -8,18 +7,16 @@ public:
     Stack( int MaxStackSize = 10);
     ~Stack() { delete []stack;}
     bool isEmpty() const {return top == -1;}
-    bool isFull() const {return tip == MaxTop;}
+    bool isFull() const {return top == MaxTop;}
     T Top() const ;
     int Size();
-    Stack<T >& Add(const T& x);
-    Stack<T >& Delete(T& x);
-    Stack<T >& inputStack(Stack< T>& otherStack);
-    Stack<T >& ouputStack(Stack< T>& otherStack);
+    void Push(const T& x);
+    void Pop(T& x);
+    void inputStack(Stack<T> otherStack);
+    Stack<T> ouputStack();
 private:
     int top;  //栈顶
     int MaxTop;  //最大的栈顶
-    int size;  //堆栈中元素的数目
     T *stack;  //堆栈元素数组
-    
 };
 #endif
