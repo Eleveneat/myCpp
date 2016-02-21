3103e5be34a35cd1e6a89a67cd0eb000e3c16756
Eleven <eleveneat@gmail.com>
2016-02-21 20:22:14 +0800

    Leetcode-2016-2-21-字符串

---
+++ LeetCode/字符串/Wildcard_Matching.cpp
@@ -0,0 +1,61 @@
+// 递归版，超时
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        return isMatch(s.c_str(), p.c_str());
+    }
+private:
+    bool isMatch(const char *s, const char *p) {
+        if (*p == '\0') return *s == '\0';
+        
+        if (*p != '*') {
+            if (*p == *s || (*p == '?' && *s != '\0'))
+                return isMatch(s + 1, p + 1);
+            else
+                return false;
+        } else {
+            while (*s != '\0') {
+                if (isMatch(s, p + 1))
+                    return true;
+                s++;
+            }
+            return *(p + 1) == '\0' ? true : false;
+        }
+    }
+};
+
+//迭代
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        return isMatch(s.c_str(), p.c_str());
+    }
+private:
+    bool isMatch(const char *s, const char *p) {
+        bool star = false;
+        const char *str, *ptr;
+        for (str = s, ptr = p; *str != '\0'; str++, ptr++) {
+            switch (*ptr) {
+                case '?':
+                    break;
+                case '*':
+                    star = true;
+                    s = str, p = ptr;
+                    while (*p == '*') p++;
+                    if (*p == '\0') return true;
+                    str = s - 1;
+                    ptr = p - 1;
+                    break;
+                default:
+                    if (*str != *ptr) {
+                        if (!star) return false;
+                        s++;
+                        str = s - 1;
+                        ptr = p - 1;
+                    }
+            }
+        }
+        while (*ptr == '*') ptr++;
+        return (*ptr == '\0');
+    }
+};
